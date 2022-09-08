96171422cf58fdc97252abac22d70ee7f7199c87
H-JWANNA <hongjjwan@gmail.com>
2022-09-08 11:05:25 +0900

    Modified 22.09.08.

--- JAVA/practice/FibonacciNum.java
+++ JAVA/practice/FibonacciNum.java
@@ -5,34 +5,44 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class FibonacciNum {
-    public static void main(String[] args) throws IOException {
-        // 입력받을 객체 생성
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    // 입력 받을 객체 생성
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-        // 안내문 출력
-        System.out.println("[안내] 피보나치 수열 프로그램 시작");
+    // 수열 개수 입력받기
+    private int getUserInput() throws IOException {
         System.out.print("원하는 수열의 개수를 입력해주세요 : ");
-        int input_num = Integer.parseInt(input.readLine());
-
-        // 수열 출력하기
-        System.out.println("[피보나치 수열 출력]");
-
-        for(int number: numbers(input_num)) {
-            System.out.println(number);
-        }
-
-        System.out.println("[안내] 프로그램을 종료합니다.");
+        return Integer.parseInt(input.readLine());
     }
 
-    // 피보나치 수열 구하는 numbers 배열
-    private static int[] numbers(int num) {
+    // 피보나치 수열 구하기
+    private int[] getFibonacci(int num) {
         int[] fib = new int[num];
 
         for (int i = 0; i < num; i++) {
             if (i <= 1) fib[i] = 1;
-            else fib[i] = fib[i-1] + fib[i-2];
+            else fib[i] = fib[i - 1] + fib[i - 2];
         }
-
         return fib;
+    }
+
+    // 수열 출력
+    private void printNumbers(int[] arr) {
+        System.out.println("[피보나치 수열 출력]");
+
+        for (int number : arr) {
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 객체 생성
+        FibonacciNum fibNum = new FibonacciNum();
+
+        System.out.println("[안내] 피보나치 수열 프로그램 시작");
+        
+        // 메서드 실행
+        fibNum.printNumbers(fibNum.getFibonacci(fibNum.getUserInput()));
+
+        System.out.println("[안내] 프로그램을 종료합니다.");
     }
 }
