f86a1596d663694f3522afd2a8571dd3f33779ba
H-JWANNA <hongjjwan@gmail.com>
2022-09-24 15:55:58 +0900

    Create StoreList

---
+++ JAVA/java_practice/chicken/program/StoreList.java
@@ -0,0 +1,11 @@
+package chicken.program;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StoreList {
+
+    public static List<String> storeName = new LinkedList<>();
+    public static List<String> storeMenu = new LinkedList<>();
+    public static List<Integer> menuPrice = new LinkedList<>();
+}
