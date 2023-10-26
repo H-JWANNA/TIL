b168dd0c989c71f794700df291557a0b8ed8d18c
H-JWANNA <hongjjwan@gmail.com>
2023-10-26 18:12:30 +0900

    initial commit

---
+++ React/practice/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  const name = "World";
+  return (
+    <div className="App">
+      <h1 style={{ color: "red", backgroundColor: "green" }}> Hello {name} </h1>
+    </div>
+  );
+}
+
+export default App;
