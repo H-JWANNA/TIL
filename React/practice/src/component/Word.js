e997d335369eeae6fbec67bbb522f1a29b6e78ff
H-JWANNA <hongjjwan@gmail.com>
2023-11-02 14:50:20 +0900

    practice PUT, DELETE

--- React/practice/src/component/Word.js
+++ React/practice/src/component/Word.js
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { useState } from "react";
+import { SERVER_URL } from "../api/getUrl";
 
-export default function Word({ word }) {
+export default function Word(props) {
+  const [word, setWord] = useState(props.word);
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
@@ -8,8 +11,33 @@ export default function Word({ word }) {
     setIsShow(!isShow);
   }
 
-  function toggleDone() {
-    setIsDone(!isDone);
+  async function toggleDone() {
+    const res = await axios.put(`${SERVER_URL}/words/${word.id}`, {
+      ...word,
+      isDone: !isDone,
+    });
+
+    // console.log("put", res);
+
+    if (res.statusText === "OK") {
+      setIsDone(!isDone);
+    }
+  }
+
+  async function del() {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      const res = await axios.delete(`${SERVER_URL}/words/${word.id}`);
+
+      // console.log("del", res);
+
+      if (res.statusText === "OK") {
+        setWord({ id: 0 });
+      }
+    }
+  }
+
+  if (word.id === 0) {
+    return null;
   }
 
   return (
@@ -21,7 +49,9 @@ export default function Word({ word }) {
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
-        <button className="btn_del">삭제</button>
+        <button onClick={del} className="btn_del">
+          삭제
+        </button>
       </td>
     </tr>
   );
